3d148db84c31f85c6c4deb01255daffb7495d24a
Evgenii Makovniuk <mmmjekammm@gmail.com>
2023-05-18 16:08:50 +0300

    Update homework 12

--- Home Work 12/index.js
+++ Home Work 12/index.js
@@ -1,143 +1,55 @@
-//---------------Price from category vegetables---------------//
-cabbage = 8;
-avocado = 30;
-tomato = 10;
+period = ['winter', 'summer'];
+categoryList = ['vegetables', 'fruits']
+vegetables = ['cabbage', 'avocado', 'tomato'];
+fruits = ['grapes', 'raspberry', 'coconut'];
+vegetablesPrices = [8, 30, 10];
+fruitsPrices = [20, 25, 50];
 
-//----------------Price from category fruits-------------------//
-grapes = 20;
-raspberry = 25;
-coconut = 50;
-
-//----------------------Questions for user----------------------//
 do { 
-    seasonBuy = prompt(
-    'What time of year do you shop: winter or summer?')
-    .toLowerCase().replaceAll(' ', '');
-} while (seasonBuy !== 'winter' && seasonBuy !== 'summer');
+    seasonBuy = prompt('What time of year do you shop: winter or summer?').toLowerCase().replaceAll(' ', '');
+} while (!period.includes(seasonBuy));
+
+seasonBuy === 'winter' ? coefficient = 2 : coefficient = 0.8;
 
 do { 
     categoryBuy = prompt(
-    'Choose category in which you want to bay products: vegetables or fruits!')
-    .toLowerCase().replaceAll(' ', '');
-} while (categoryBuy !== 'vegetables' && categoryBuy !== 'fruits');  
+    'Choose category in which you want to bay products: vegetables or fruits!').toLowerCase().replaceAll(' ', '');
+} while (!categoryList.includes(categoryBuy));  
 
 switch (categoryBuy) {
     case 'vegetables':
         do {
-            productOfVegetables = prompt(
-            'Choose product from category vegetables: cabbage, avocado, tomato.')
-            .toLowerCase().replaceAll(' ', '');
-        } while (productOfVegetables !== 'cabbage' && productOfVegetables !== 'avocado' 
-            && productOfVegetables !== 'tomato');
+            product = prompt(
+            'Choose product from category vegetables: cabbage, avocado, tomato.').toLowerCase().replaceAll(' ', '');
+            priceOfProduct = vegetablesPrices[vegetables.indexOf(product)];
+        } while (!vegetables.includes(product));
         do {
-            countOfProduct = parseInt(prompt(`Enter count of ${productOfVegetables}`));
+            countOfProduct = parseInt(prompt(`Enter count of ${product}`));
         } while (countOfProduct < 2 || isNaN(countOfProduct));
         break;
     case 'fruits':
         do {
-            productOfFruits = prompt(
-            'Choose product from category fruits: grapes, raspberry, coconut.')
-            .toLowerCase().replaceAll(' ', '');
-        } while (productOfFruits !== 'grapes' && productOfFruits !== 'raspberry' 
-           && productOfFruits !== 'coconut');
+            product = prompt(
+            'Choose product from category fruits: grapes, raspberry, coconut.').toLowerCase().replaceAll(' ', '');
+            priceOfProduct = fruitsPrices[fruits.indexOf(product)];
+        } while (!fruits.includes(product));
         do {
-            countOfProduct = parseInt(prompt(`Enter count of ${productOfFruits}`));
+            countOfProduct = parseInt(prompt(`Enter count of ${product}`));
         } while (countOfProduct < 2 || isNaN(countOfProduct));   
         break;
 } 
 
-//----------------------------Switch and case for productOfVegetables------------------------//
-if (categoryBuy === 'vegetables') {
-    switch (productOfVegetables) {
-        case 'cabbage':
-            document.write(`
-                <div class="product" align="center">
-                  <img src="images/vegetables/cabbage.svg" alt="cabbage" width="100" height="100">
-                  <p>Selected product: <b>${productOfVegetables}</b></p>
-                  <p>Count of ${productOfVegetables}: <b>${countOfProduct}</b></p>
-                  <p>Selected period: <b>${seasonBuy}</b></p>
-                  <p>Selected category: <b>${categoryBuy}</b></p>
-                  <p>Final sum: <b>${seasonBuy === 'winter'
-                  ? cabbage * 2 * countOfProduct 
-                  : cabbage * 0.8 * countOfProduct} UAH</b></p>
-                </div>
-            `);
-            break;
-        case 'avocado':
-            document.write(`
-                <div class="product" align="center">
-                  <img src="images/vegetables/avocado.svg" alt="avocado" width="100" height="100">
-                  <p>Selected product: <b>${productOfVegetables}</b></p>
-                  <p>Count of ${productOfVegetables}: <b>${countOfProduct}</b></p>
-                  <p>Selected period: <b>${seasonBuy}</b></p>
-                  <p>Selected category: <b>${categoryBuy}</b></p>
-                  <p>Final sum: <b>${seasonBuy === 'winter'
-                  ? avocado * 2 * countOfProduct 
-                  : avocado * 0.8 * countOfProduct} UAH</b></p>
-                </div>
-            `);
-             break;
-        case 'tomato':
-            document.write(`
-                <div class="product" align="center">
-                  <img src="images/vegetables/tomato.svg" alt="tomato" width="100" height="100">
-                  <p>Selected product: <b>${productOfVegetables}</b></p>
-                  <p>Count of ${productOfVegetables}: <b>${countOfProduct}</b></p>
-                  <p>Selected period: <b>${seasonBuy}</b></p>
-                  <p>Selected category: <b>${categoryBuy}</b></p>
-                  <p>Final sum: <b>${seasonBuy === 'winter'
-                  ? tomato * 2 * countOfProduct 
-                  : tomato * 0.8 * countOfProduct} UAH</b></p>
-                </div>
-           `);
-            break;
-    }  
-}  
+finalSum = countOfProduct *  priceOfProduct * coefficient;
 
-//-------------------------------Switch and case for productOfFruits-------------------------//
-else {
-    switch (productOfFruits) {
-        case 'coconut':
-            document.write(`
-                <div class="product" align="center">
-                  <img src="images/fruits/coconut.svg" alt="coconut" width="100" height="100">
-                  <p>Selected product: <b>${productOfFruits}</b></p>
-                  <p>Count of ${productOfFruits}: <b>${countOfProduct}</b></p>
-                  <p>Selected period: <b>${seasonBuy}</b></p>
-                  <p>Selected category: <b>${categoryBuy}</b></p>
-                  <p>Final sum: <b>${seasonBuy === 'winter'
-                  ? coconut  * 2 * countOfProduct 
-                  : coconut  * 0.8 * countOfProduct} UAH</b></p>
-                </div>
-             `);
-             break;
-        case 'grapes':
-            document.write(`
-                <div class="product" align="center">
-                  <img src="images/fruits/grapes.svg" alt="grapes" width="100" height="100">
-                  <p>Selected product: <b>${productOfFruits}</b></p>
-                  <p>Count of ${productOfFruits}: <b>${countOfProduct}</b></p>
-                  <p>Selected period: <b>${seasonBuy}</b></p>
-                  <p>Selected category: <b>${categoryBuy}</b></p>
-                  <p>Final sum: <b>${seasonBuy === 'winter'
-                  ? grapes * 2 * countOfProduct 
-                  : grapes * 0.8 * countOfProduct} UAH</b></p>
-                </div>
-             `);
-             break;
-        case 'raspberry':
-            document.write(`
-                <div class="product" align="center">
-                  <img src="images/fruits/raspberry.svg" alt="raspberry" width="100" height="100">
-                  <p>Selected product: <b>${productOfFruits}</b></p>
-                  <p>Count of ${productOfFruits}: <b>${countOfProduct}</b></p>
-                  <p>Selected period: <b>${seasonBuy}</b></p>
-                  <p>Selected category: <b>${categoryBuy}</b></p>
-                  <p>Final sum: <b>${seasonBuy === 'winter'
-                  ? raspberry * 2 * countOfProduct 
-                  : raspberry * 0.8 * countOfProduct} UAH</b></p>
-                </div>
-            `);
-            break;
-    }
-}
+imageProduct = `<img src="images/${categoryBuy}/${product}.svg" alt="${product}" width="100" height="100">`;
+
+document.write(`
+    <div class="product" align="center">
+        ${imageProduct}
+        <p>Selected product: <b>${product}</b></p>
+        <p>Count of ${product}: <b>${countOfProduct}</b></p>
+        <p>Selected period: <b>${seasonBuy}</b></p>
+        <p>Selected category: <b>${categoryBuy}</b></p>
+        <p>Final sum: <b>${finalSum} UAH</b></p>
+    </div>
+`);
---
+++ Home Work 13/index.js
@@ -0,0 +1,128 @@
+newArray = [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+console.log(newArray);
+//1)--------Знайти суму та кількість позитивних елементів.-------//
+resultSumPositiNumber = 0;
+countPositivNumber = 0;
+
+for (i = 0; i < newArray.length; i++) {
+    if (newArray[i] > 0){
+        resultSumPositiNumber += newArray[i];
+        countPositivNumber++;
+    }
+}
+
+console.log(`Sum of positive number of array is: ${resultSumPositiNumber}`);
+console.log(`Count of positive number of array is: ${countPositivNumber}`);
+
+//2)-----Знайти мінімальний елемент масиву та його порядковий номер.----//
+minElement = 0;
+
+for (i = 0; i < newArray.length; i++) {
+       if (newArray[i] < minElement) {
+        minElement = newArray[i];
+       }       
+}
+
+serialNumberMinElement = newArray.indexOf(minElement);
+
+console.log(`Mimimal elenment of array is: ${minElement}`);
+console.log(`Serial number minimal element of array is: ${serialNumberMinElement}`);
+
+//3)----Знайти максимальний елемент масиву та його порядковий номер.----//
+maxElement = 0;
+
+for (i = 0; i < newArray.length; i++) {
+       if (newArray[i] > maxElement) {
+        maxElement = newArray[i];
+       }       
+}
+
+serialNumberMaxElement = newArray.indexOf(maxElement);
+
+console.log(`Maximum elenment of array is: ${maxElement}`);
+console.log(`Serial number maximum element of array is: ${serialNumberMaxElement}`);
+
+//4)---------Визначити кількість негативних елементів.--------//
+countNegativeElements = 0;
+
+for (i = 0; i < newArray.length; i++) {
+    if (newArray[i] < 0) {
+        countNegativeElements++;
+    }
+}
+
+console.log(`Count of negative elements of array is: ${countNegativeElements}`);
+
+//5)---------Знайти кількість непарних позитивних елементів.--------//
+countUnpairPositiveElements = 0;
+
+for (i = 0; i < newArray.length; i++) {
+    if (newArray[i] > 0 && newArray[i] % 2 !== 0) {
+        countUnpairPositiveElements ++;
+    }
+}
+
+console.log(`Count of positive unpair  elements of array is: ${countUnpairPositiveElements}`);
+
+//6)---------Знайти кількість парних позитивних елементів.-------//
+countPairPositiveElements = 0;
+
+for (i = 0; i < newArray.length; i++) {
+    if (newArray[i] > 0 && newArray[i] % 2 === 0) {
+        countPairPositiveElements++;
+    }
+}
+
+console.log(`Count of positive pair  elements of array is: ${countPairPositiveElements}`);
+
+//7)---------Знайти суму парних позитивних елементів..-------//
+sumPairPositiveElements = 0;
+
+for (i = 0; i < newArray.length; i++) {
+    if (newArray[i] > 0 && newArray[i] % 2 === 0) {
+        sumPairPositiveElements += newArray[i];
+    }
+}
+
+console.log(`Sum of positive pair  elements of array is: ${sumPairPositiveElements}`);
+
+
+//8)---------Знайти суму непарних позитивних елементів.-------//
+sumUnpairPositiveElements = 0;
+
+for (i = 0; i < newArray.length; i++) {
+    if (newArray[i] > 0 && newArray[i] % 2 !== 0) {
+        sumUnpairPositiveElements += newArray[i];
+    }
+}
+
+console.log(`Sum of positive unpair  elements of array is: ${sumUnpairPositiveElements}`);
+
+//9)---------Знайти добуток позитивних елементів..-------//
+multiplyPositiveElements = 1;
+
+for (i = 0; i < newArray.length; i++) {
+    if (newArray[i] > 0) {
+        multiplyPositiveElements *= newArray[i];
+    }
+}
+
+console.log(`Sum of positive unpair  elements of array is: ${multiplyPositiveElements}`);
+
+//10)---------Знайти найбільший серед елементів масиву, остальні обнулити.-------//
+maxElement = 0;
+
+for (i = 0; i < newArray.length; i++) {
+       if (newArray[i] > maxElement) {
+        maxElement = newArray[i];
+       }       
+}
+newArray2 = [];
+for (i = 0; i < newArray.length; i++) {
+    newArray[i] !== maxElement 
+    ? newArray2.push(newArray[i] = 0)
+    : newArray2.push(newArray[i])
+          
+}
+console.log(newArray2);
+console.log(`Your new zeroed array: ${newArray2}`);
