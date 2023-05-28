7d16d73b866f45203458e6129834c42dd8b26734
Evgenii Makovniuk <mmmjekammm@gmail.com>
2023-05-28 19:51:17 +0300

    update

--- Home Work 18/index.js
+++ Home Work 18/index.js
@@ -1,11 +1,12 @@
 const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
 function generateKey(length, characters) {
-	arrayKeys= [];
-	for (let i = 0; i < length; i++) {
-		arrayKeys.push(characters.split('')[(Math.round(Math.random() * ((characters.length - 1) - 0) + 0))]);
-	}
-	return arrayKeys.join('');
+  arrayKey= [];
+  for (let i = 0; i < length; i++) {
+	arrayKey.push(characters.split('')[(Math.round(Math.random() * ((characters.length - 1) - 0) + 0))]);
+  }
+
+  return arrayKey.join('');
 }
 
 const key = generateKey(10, characters);
