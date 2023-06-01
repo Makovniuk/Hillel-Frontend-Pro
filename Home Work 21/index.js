f56fb97e4f50837f80702a4c7a57d40c439028f2
Evgenii Makovniuk <mmmjekammm@gmail.com>
2023-06-02 01:42:26 +0300

    deep clone

--- Home Work 21/index.js
+++ Home Work 21/index.js
@@ -1,75 +1,26 @@
-// 1)Дано масив з елементами різних типів. 
-// // Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.
+const newArray = [
+  [
+    ['some text', true, [1,2,3,4, [10, 20]]],
+  ],
+  [
+    [1, 2, 3, [100, 200]],
+    ['name', 'age']
+  ],
+];
 
-// newArray = [25, 60, true, 'string', false, 15, null, undefined, 500, -360];
+function deepClone(array) {
+  const coppyArray = [];
 
-// function calcArithmeticMean(array) {
-//    let count = 0;
-//    let sum = 0;
-//   for (let i = 0; i < array.length; i++) {
-//     if (typeof array[i] === 'number') {
-//       count++;
-//       sum += array[i];
-//     }
-//   }
+  for (let i = 0; i< array.length; i++) {
+    if (Array.isArray(array[i])) {
+        const recursiveClone = deepClone(array[i]);
+        coppyArray.push(recursiveClone);
+    } else {
+      coppyArray.push(array[i]);
+    }
+  }
 
-//   return sum / count;
-// }
+  return coppyArray;
+}
 
-// const ArithmeticMean = calcArithmeticMean(newArray);
-// console.log(ArithmeticMean);
-
-
-//2)Написати функцію doMath(x, znak, y), яка отримує 3 аргументи: числа x і y, рядок znak. 
-//У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).
-// //Вивести результат математичної дії, вказаної в змінній znak.Обидва числа і знак виходять від користувача.
-// debugger
-
-// function doMath(x, znak, y) {
-//  x = +prompt('Enter first number!');
-//  znak = prompt('Enter  mathematical operation: +, -, *, /, %, ^ ');
-//  y = +prompt('Enter second number!');
-
-//  let result;
-
-//   switch (znak) {
-//       case "+":
-//           result = x + y;
-//           break;
-//       case "-":
-//           result = x - y;
-//           break;
-//       case "*":
-//           result = x * y;
-//           break;
-//       case "/":
-//           result = x / y;
-//           break;  
-//       case "%":
-//           result = x % y;
-//           break;
-//       case "^":
-//           result = Math.pow(x,y);
-//           break; 
-//       default:
-//         result = 'Sorry, you entered not correct values!'       
-//   }
-
-//   return result;
-// }
-
-// let resultDoMath = doMath();
-// console.log(resultDoMath);
-
-// 3)Написати функцію заповнення даними користувача двомірного масиву.
-// Довжину основного масиву і внутрішніх масивів задає користувач. 
-// // Значення всіх елементів всіх масивів задає користувач.
-
-
-// function sum(number) {
-//     let values = number;
-//     function ader() {
-
-
-//     }
-// 
+ console.log(deepClone(newArray));
